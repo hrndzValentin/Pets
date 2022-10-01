9dcb09b0fa8d143989f2d22b49cb23db66168329
hrndzValentin <91339201+hrndzValentin@users.noreply.github.com>
2022-10-01 15:31:23 -0500

    first commit

---
+++ PetsApi/Dtos/CustomerDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetsApi.Dtos
+{
+    public class CustomerDto
+    {
+        public long Id { get; set; }
+       
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ PetsApi/Repositories/Persona.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PetsApi.Repositories
+{
+    public class Persona : DbContext
+    {
+    }
+}
